f7ac8d599b3766e2cbb15560c560077f28aa9737
-Arman.Aghighi <arman.aghighi@gmail.com>
2023-10-10 14:32:01 +0330

    Factorial and Fibonacci Dynamic programming example add
    solve 2 example of dynamic programming

---
+++ Algorithms and Data Structures/Fibonachi Dynamic/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fib_dynamic
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            UIClass.ShowMessage();
+            Logic logic = new Logic();
+            UIClass.ShowResualt(logic);
+        }
+    }
+}
